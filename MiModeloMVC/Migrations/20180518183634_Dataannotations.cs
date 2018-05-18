80c259173378edc59299fe334539c1da8e48c3a8
Edd Osorio <eosorio@vsys.com>
2018-05-18 13:54:49 -0500

    Added Mirations to MiModeloMVC

---
+++ MiModeloMVC/Migrations/20180518183634_Dataannotations.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MiModeloMVC.Migrations
+{
+    public partial class Dataannotations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    edad = table.Column<int>(nullable: false),
+                    fechaAlta = table.Column<DateTime>(nullable: false),
+                    nombre = table.Column<string>(maxLength: 60, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
